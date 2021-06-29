213ab748e5b0a75b341eab7f6a2b1e44daadbfa3
Eltonleao <3lton.leao@gmail.com>
2021-06-29 20:42:16 -0300

    criando classe App com model separada e loading do database em json

---
+++ js/core/model.js
@@ -0,0 +1,23 @@
+const Database = function(siteData){
+    var env = this;
+    this.siteData = siteData;
+    this.database;
+
+    this.loadDatabase = function(){
+        try {
+            
+            return $.ajax({
+                type: "GET",
+                url: env.siteData.url,
+                success: function(result){
+                    return result;
+                },
+              });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export default Database;

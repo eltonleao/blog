3b034b1cbd6a660e8c9feecc84c8714889c4d8ba
eltonleao <3lton.leao@gmail.com>
2022-05-08 16:18:33 -0300

    .

--- js/core/View.js
+++ js/core/View.js
@@ -2,32 +2,32 @@ const View = function (siteData) {
     var env = this;
     this.siteData = siteData;
 
-    document.title = 'eltonleao | blog';
+    // document.title = 'eltonleao | blog';
 
     this.renderNavbar = function () {
         let html = "";
         html += '    <div class="container">';
         html += '  <a class="navbar-brand" href="/">eltonleao</a>';
-        html += '  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"';
-        html += '    data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"';
-        html += '    aria-label="Toggle navigation">';
-        html += '    Menu';
-        html += '    <i class="fas fa-bars"></i>';
-        html += '  </button>';
+        // html += '  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"';
+        // html += '    data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"';
+        // html += '    aria-label="Toggle navigation">';
+        // html += '    Menu';
+        // html += '    <i class="fas fa-bars"></i>';
+        // html += '  </button>';
         html += '  <div class="collapse navbar-collapse" id="navbarResponsive">';
         html += '    <ul class="navbar-nav ml-auto">';
-        html += '      <li class="nav-item">';
-        html += '        <a class="nav-link" href="index.html">Home</a>';
-        html += '      </li>';
+        // html += '      <li class="nav-item">';
+        // html += '        <a class="nav-link" href="index.html">Home</a>';
+        // html += '      </li>';
         // html += '      <li class="nav-item">';
         // html += '        <a class="nav-link" href="post.html">Sample Post</a>';
         // html += '      </li>';
         // html += '      <li class="nav-item">';
         // html += '        <a class="nav-link" href="contact.html">Contact</a>';
         // html += '      </li>';
-        html += '      <li class="nav-item">';
-        html += '        <a class="nav-link" href="https://eltonleao.github.io">About</a>';
-        html += '      </li>';
+        // html += '      <li class="nav-item">';
+        // html += '        <a class="nav-link" href="https://eltonleao.github.io">About</a>';
+        // html += '      </li>';
         html += '    </ul>';
         html += '  </div>';
         html += '</div>';
